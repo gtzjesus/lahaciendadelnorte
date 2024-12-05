3ab73e6b43548d6b4278252c80eba9dba6d067b9
gtz.jesus <gtz.jesus@outlook.com>
2024-12-04 17:45:23 -0700

    Implementation of helper function getAllProducts with GROQ query working + live backend frontend connection

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,5 +1,8 @@
 // import { Button } from '@/components/ui/button';
 
-export default function Home() {
+import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
+
+export default async function Home() {
+  const products = await getAllProducts();
   return <div>{/* <Button>start for free</Button> */}</div>;
 }
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,17 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from '../env';
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  useCdn: true,
+  // stega allows visual editor to encode page
+  // Coded dynamically for development and production
+  stega: {
+    studioUrl: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  },
+});
