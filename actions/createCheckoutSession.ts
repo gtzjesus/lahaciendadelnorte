b1b1014be860500fc10b649a1e6a6139fb991c28
gtz.jesus <gtz.jesus@outlook.com>
2024-12-10 03:52:18 -0700

    checkout session completed + new success page redirected successful!

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -38,6 +38,16 @@ export async function createCheckoutSession(
       customerId = customers.data[0].id;
     }
 
+    // create urls
+    const baseUrl =
+      process.env.NODE_ENV === 'production'
+        ? `https://${process.env.VERCEL_URL}`
+        : `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+    const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
+
+    const cancelUrl = `${baseUrl}/basket`;
+
     // create checkout session
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
@@ -46,8 +56,8 @@ export async function createCheckoutSession(
       metadata,
       mode: 'payment',
       allow_promotion_codes: true,
-      success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-      cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+      success_url: successUrl,
+      cancel_url: cancelUrl,
       line_items: items.map((item) => ({
         price_data: {
           currency: 'usd',
