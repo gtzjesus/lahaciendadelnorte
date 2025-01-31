d71d2f810cca8cf8d7e23c928ccc6f797e6c316a
gtz.jesus <gtz.jesus@outlook.com>
2025-01-31 06:33:51 -0700

    header modification + menu added with transitions

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,23 @@
 'use client';
+
 import { ClerkLoaded, SignInButton, UserButton, useUser } from '@clerk/nextjs';
 import Link from 'next/link';
 import Form from 'next/form';
 import { PackageIcon, TrolleyIcon } from '@sanity/icons';
 import useBasketStore from '../../store/store';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
   const { user } = useUser();
   const itemCount = useBasketStore((state) =>
     state.items.reduce((total, item) => total + item.quantity, 0)
   );
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const createClerkPasskey = async () => {
     try {
@@ -19,45 +27,56 @@ export default function Header() {
       console.error('Error', JSON.stringify(err, null, 2));
     }
   };
+
+  // Disable scrolling when the menu is open
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto'; // Enable scrolling when the menu is closed
+    }
+
+    // Clean up the effect when the component is unmounted
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isMenuOpen]);
+
   return (
-    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+    <header className="flex flex-wrap justify-between items-center px-8 py-4 relative">
       <div className="flex w-full flex-wrap justify-between items-center">
         <Link
           href="/"
-          className="font-bold hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+          className="font-bold hover:opacity-50 cursor-pointer sm:mx-0"
         >
-          nextcommerce
+          Nextcommerce
         </Link>
-        <Form
-          action="/search"
-          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
-        >
-          <input
-            type="text"
-            name="query"
-            placeholder="search for products"
-            className="bg-gray-100 text-gray-800 px-4 py-4 rounded focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 border w-full max-w-4xl"
-          />
-        </Form>
 
-        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 md:flex-none">
+        {/* Toggle Menu Button */}
+        <button
+          onClick={toggleMenu}
+          className="sm:hidden flex items-center space-x-2 text-black font-bold"
+        >
+          Menu
+        </button>
+
+        {/* Desktop Menu (always visible) */}
+        <div className="hidden sm:flex items-center space-x-4 mt-4 sm:mt-0 flex-1 md:flex-none">
           <Link
             href="/basket"
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
+            className="relative flex justify-center items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
           >
             <TrolleyIcon className="w-5 h-5" />
-
             <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
               {itemCount}
             </span>
           </Link>
 
-          {/* User Area */}
           <ClerkLoaded>
             {user && (
               <Link
                 href="/orders"
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
+                className="relative flex justify-center items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
               >
                 <PackageIcon className="w-5 h-5" />
               </Link>
@@ -66,7 +85,6 @@ export default function Header() {
             {user ? (
               <div className="flex items-center space-x-2">
                 <UserButton />
-
                 <div className="hidden sm:block text-xs">
                   <p className="text-gray-400 font-bold">{user.fullName}</p>
                 </div>
@@ -75,7 +93,79 @@ export default function Header() {
               <SignInButton mode="modal" />
             )}
 
-            {/* Passkey Area */}
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-opacity-90 hover:text-black animate-pulse text-black font-bold py-2 px-4 rounded border-black-300 border"
+              >
+                create passkey
+              </button>
+            )}
+          </ClerkLoaded>
+        </div>
+      </div>
+
+      {/* Backdrop (Mobile) */}
+      <div
+        className={`fixed inset-0 bg-black bg-opacity-50 z-10 transition-opacity duration-300 ${
+          isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+        onClick={toggleMenu}
+      />
+
+      {/* Sliding Menu (Mobile) */}
+      <div
+        className={`fixed right-0 top-0 h-full w-full bg-white shadow-xl z-20 transform transition-opacity duration-300 ${
+          isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+      >
+        {/* Close Button (X) */}
+        <button
+          onClick={toggleMenu}
+          className="absolute top-4 right-4 text-2xl text-gray-600"
+        >
+          &times;
+        </button>
+
+        <div className="flex flex-col space-y-6 p-6">
+          <Form action="/search" className="w-full">
+            <input
+              type="text"
+              name="query"
+              placeholder="search for products"
+              className="bg-gray-100 text-gray-800 px-4 py-4 rounded focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 border w-full"
+            />
+          </Form>
+
+          <Link
+            href="/basket"
+            className="flex items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
+          >
+            <TrolleyIcon className="w-5 h-5" />
+            <span className="text-xs">{itemCount}</span>
+          </Link>
+
+          <ClerkLoaded>
+            {user && (
+              <Link
+                href="/orders"
+                className="flex items-center space-x-2 bg-black hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded"
+              >
+                <PackageIcon className="w-5 h-5" />
+              </Link>
+            )}
+
+            {user ? (
+              <div className="flex items-center space-x-2">
+                <UserButton />
+                <div className="text-xs">
+                  <p className="text-gray-400 font-bold">{user.fullName}</p>
+                </div>
+              </div>
+            ) : (
+              <SignInButton mode="modal" />
+            )}
+
             {user?.passkeys.length === 0 && (
               <button
                 onClick={createClerkPasskey}
