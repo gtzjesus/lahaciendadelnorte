b266b4c30bf9d53d300f54e41196754a641d5dad
gtz.jesus <gtz.jesus@outlook.com>
2025-02-06 05:36:57 -0700

    Documentation added, header looking finished with these final mods

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+// Importing necessary components and hooks from the Clerk library for authentication and user management.
 import { ClerkLoaded, SignInButton, UserButton, useUser } from '@clerk/nextjs';
 import Link from 'next/link';
-import useBasketStore from '../../store/store';
+import useBasketStore from '../../store/store'; // Import custom store for basket items
 import { useState, useEffect } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import Image from 'next/image';
 
+// CartButton component displays the cart icon and the number of items in the basket
 const CartButton = ({ itemCount }: { itemCount: number }) => (
   <Link
     href="/basket"
@@ -20,24 +22,29 @@ const CartButton = ({ itemCount }: { itemCount: number }) => (
       className="w-6 h-6 opacity-70"
     />
     {itemCount > 0 && (
+      // Displays the item count as a small badge on the cart icon if there are items
       <span className="absolute opacity-55 -top-0.5 bg-custom-gray text-black rounded-full w-3 h-3 flex items-center justify-center text-[8px] font-bold transition-all duration-200 ease-in-out">
         {itemCount}
       </span>
     )}
   </Link>
 );
+
+// AuthButtons component handles user authentication states, showing different buttons based on whether the user is signed in or not
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const AuthButtons = ({ user }: { user: any }) => (
   <ClerkLoaded>
     {user ? (
       <>
+        {/* Link to user orders */}
         <Link
           href="/orders"
-          className="flex items-center space-x-2 opacity-60 text-black font-bold py-2 px-4 rounded"
+          className="flex items-center space-x-2 opacity-70 text-black font-bold py-2 px-4 rounded"
         >
           <span>orders</span>
         </Link>
 
+        {/* User profile button */}
         <div className="flex items-center space-x-2">
           <div className="relative">
             <UserButton />
@@ -49,6 +56,7 @@ const AuthButtons = ({ user }: { user: any }) => (
         </div>
       </>
     ) : (
+      // SignInButton renders if user is not logged in
       <div className="opacity-60">
         <SignInButton mode="modal" />
       </div>
@@ -57,36 +65,44 @@ const AuthButtons = ({ user }: { user: any }) => (
 );
 
 const Header = () => {
+  // Fetching user data from Clerk
   const { user } = useUser();
+
+  // Basket item count calculation using custom store
   const itemCount = useBasketStore((state) =>
     state.items.reduce((total, item) => total + item.quantity, 0)
   );
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMounted, setIsMounted] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-  const [windowWidth, setWindowWidth] = useState<number>(0); // Initialize as 0
+  // State management for various UI functionalities
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Controls the visibility of the mobile menu
+  const [isMounted, setIsMounted] = useState(false); // Ensures the code runs on the client-side
+  const [scrolled, setScrolled] = useState(false); // Tracks the scroll position to apply different header styles
+  const [windowWidth, setWindowWidth] = useState<number>(0); // Keeps track of window width for responsive design
 
+  // Router and pathname hooks
   const router = useRouter();
   const pathname = usePathname();
 
-  // Ensuring the code runs only on the client side
+  // Effect hook to ensure the component mounts on the client-side
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  // Handling scroll and window resize events
+  // Effect hook to manage scroll and window resize events
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const handleScroll = () => {
+        // Add a shadow to header when scrolling past 50px
         setScrolled(window.scrollY > 50);
       };
 
-      const handleResize = () => setWindowWidth(window.innerWidth);
+      const handleResize = () => setWindowWidth(window.innerWidth); // Update window width on resize
 
+      // Adding event listeners for scroll and resize
       window.addEventListener('scroll', handleScroll);
       window.addEventListener('resize', handleResize);
 
+      // Clean-up function for event listeners
       return () => {
         window.removeEventListener('scroll', handleScroll);
         window.removeEventListener('resize', handleResize);
@@ -94,19 +110,22 @@ const Header = () => {
     }
   }, []);
 
+  // Close the menu when resizing to a desktop view (width >= 648px)
   useEffect(() => {
-    // Close menu when resizing to desktop view
     if (windowWidth >= 648) {
       setIsMenuOpen(false);
     }
   }, [windowWidth]);
 
+  // Toggle menu visibility (mobile view)
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
+  // Close the menu when pathname changes (navigation)
   useEffect(() => {
     setIsMenuOpen(false);
   }, [pathname]);
 
+  // Manage body overflow based on menu visibility to prevent scrolling when menu is open
   useEffect(() => {
     document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
     return () => {
@@ -114,23 +133,25 @@ const Header = () => {
     };
   }, [isMenuOpen]);
 
+  // Handle search submission by redirecting to the search page
   const handleSearchSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     const form = e.target as HTMLFormElement;
     const query = new FormData(form).get('query') as string;
 
-    setIsMenuOpen(false);
+    setIsMenuOpen(false); // Close mobile menu after search
     router.push(`/search?query=${query}`);
 
     const inputElement = form.querySelector(
       'input[name="query"]'
     ) as HTMLInputElement;
     if (inputElement) {
-      inputElement.blur();
+      inputElement.blur(); // Blur the input field after submitting
     }
   };
 
+  // Prevent rendering until the component is fully mounted (client-side)
   if (!isMounted) return null;
 
   return (
@@ -163,6 +184,7 @@ const Header = () => {
             <span className="font-bold text-md opacity-70">nextcommerce</span>
           </div>
 
+          {/* Search Bar: Visible on larger screens */}
           <form
             onSubmit={handleSearchSubmit}
             className="hidden sm:flex items-center w-1/2"
@@ -185,6 +207,7 @@ const Header = () => {
           </form>
         </div>
 
+        {/* Right side: Cart and Auth Buttons */}
         <div className="flex items-center ">
           <CartButton itemCount={itemCount} />
           <div className="hidden sm:flex items-center space-x-4">
@@ -192,6 +215,7 @@ const Header = () => {
           </div>
         </div>
 
+        {/* Mobile Menu Button */}
         <button
           onClick={toggleMenu}
           className="sm:hidden flex flex-col justify-center items-center space-y-1 z-30 relative group"
@@ -205,11 +229,13 @@ const Header = () => {
         </button>
       </div>
 
+      {/* Mobile Menu Overlay */}
       <div
         className={`fixed inset-0 bg-black bg-opacity-50 z-10 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
         onClick={toggleMenu}
       />
 
+      {/* Mobile Menu */}
       <div
         className={`fixed right-0 top-0 h-full w-full bg-white shadow-xl z-20 transform transition-opacity duration-300 ease-in-out ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
       >
@@ -218,6 +244,7 @@ const Header = () => {
           className="absolute top-3 right-7 text-3xl text-gray-600"
         />
         <div className="flex flex-col space-y-6 p-16">
+          {/* Mobile Search */}
           <form onSubmit={handleSearchSubmit} className="w-full">
             <div className="flex items-center px-4 py-4 rounded-lg bg-gray-50">
               <Image
@@ -230,12 +257,13 @@ const Header = () => {
               <input
                 type="search"
                 name="query"
-                placeholder="search"
+                placeholder="Search"
                 className="w-full caret-blue-500 focus:outline-none bg-transparent placeholder:text-md appearance-none"
               />
             </div>
           </form>
 
+          {/* Mobile Auth Buttons */}
           <AuthButtons user={user} />
         </div>
       </div>
