9996de502c6bb4566a3d04d6615037e46822a238
gtz.jesus <gtz.jesus@outlook.com>
2025-02-13 17:54:13 -0700

    Changing layout product grid to display 2-at-a-time in mobile responviseness rather than 1! supe change

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -5,22 +5,20 @@ import ProductThumb from './ProductThumb';
 
 function ProductGrid({ products }: { products: Product[] }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-      {products?.map((product) => {
-        return (
-          <AnimatePresence key={product._id}>
-            <motion.div
-              layout
-              initial={{ opacity: 0.2 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              className="flex justify-center"
-            >
-              <ProductThumb key={product._id} product={product} />
-            </motion.div>
-          </AnimatePresence>
-        );
-      })}
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+      {products?.map((product) => (
+        <AnimatePresence key={product._id}>
+          <motion.div
+            layout
+            initial={{ opacity: 0.2 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex justify-center"
+          >
+            <ProductThumb key={product._id} product={product} />
+          </motion.div>
+        </AnimatePresence>
+      ))}
     </div>
   );
 }
