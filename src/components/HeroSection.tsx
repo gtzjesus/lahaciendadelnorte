b67591231d16b75d33e263edd93c8bd0d38b8363
gtz.jesus <gtz.jesus@outlook.com>
2025-02-19 18:52:49 -0700

    Hero section added + modded for video background

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,26 +1,26 @@
 // components/HeroSection.tsx
 
-import Link from 'next/link';
+// import Link from 'next/link';
 import { FC } from 'react';
 
 const HeroSection: FC = () => {
   return (
-    <section className="absolute inset-0 flex items-center justify-center z-10 text-white text-center px-6 sm:px-12">
+    <section className="absolute inset-0 flex items-center justify-center z-10 text-white text-center ">
       <div className="space-y-4 max-w-3xl">
-        <h1 className="text-4xl sm:text-5xl font-extrabold leading-tight">
-          Inventory Available Now
+        <h1 className="text-3xl sm:text-4xl font-extrabold leading-tight">
+          Nextcommerce
         </h1>
-        <p className="text-lg sm:text-xl font-medium">
-          Explore Reduced Pricing and Get the Best Deals!
+        <p className="text-sm sm:text-xl font-medium">
+          nextjs | sanity | clerk | vercel | stripe
         </p>
 
         {/* Call-to-action Button */}
-        <Link
+        {/* <Link
           href="/inventory"
           className="inline-block mt-4 px-6 py-3 bg-yellow-500 text-black font-bold text-lg rounded-lg shadow-lg hover:bg-yellow-600 transition-all duration-300"
         >
           View Inventory
-        </Link>
+        </Link> */}
       </div>
     </section>
   );
