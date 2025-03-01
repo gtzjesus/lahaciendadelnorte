2bec73f0b29fdd3f483eefd7cb3faca944c8b1a1
gtz.jesus <gtz.jesus@outlook.com>
2025-03-01 10:31:09 -0700

    Footer implementation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,21 @@ import Image from 'next/image'; // Import Next.js image component
 
 const Footer = () => {
   return (
-    <footer className="  py-4">
+    <footer className="  py-6">
       <div className="max-w-7xl mx-auto text-center">
         {/* Logo */}
         <div className="flex flex-col items-center mb-4">
           <Image
             src="/icons/logo.webp" // Path to your logo image
             alt="Your Logo"
-            width={30} // Customize based on your logo's size
-            height={30} // Customize based on your logo's size
+            width={25} // Customize based on your logo's size
+            height={25} // Customize based on your logo's size
           />
           <p className="text-sm text-black">Nextcommerce</p>
         </div>
 
         {/* Copyright Text */}
-        <p className="text-sm text-black">
+        <p className="text-xs text-black">
           &copy; WorldHello {new Date().getFullYear()}. All rights reserved.
         </p>
       </div>
