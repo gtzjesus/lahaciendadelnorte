c46ffe80fb46d80bc44ddfa0bba2aa7f8d65e7d1
gtz.jesus <gtz.jesus@outlook.com>
2025-03-24 20:43:55 -0600

    Product page implementation

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import AddToBasketButton from '@/components/cart/AddToBasketButton';
+import Header from '@/components/common/header';
 import { Button } from '@/components/ui/button';
 import { imageUrl } from '@/lib/imageUrl';
 import { getProductBySlug } from '@/sanity/lib/products/getProductBySlug';
@@ -22,45 +23,52 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
   const isOutOfStock = product.stock != null && product.stock <= 0;
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="flex flex-col space-y-4">
-          {/* Loop through the extraImages and display them */}
-          {product.extraImages?.map((image, index) => (
-            <div
-              key={index}
-              className={`relative aspect-square overflow-hidden rounded-lg shadow-lg ${isOutOfStock ? 'opacity-50' : ''}`}
-            >
-              <Image
-                src={imageUrl(image).url()}
-                alt={`${product.name} extra image ${index + 1}`}
-                fill
-                className="object-contain transition-transform duration-300 hover:scale-105"
-              />
-            </div>
-          ))}
-          {isOutOfStock && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-              <span className="text-white font-bold text-lg">Out of Stock</span>
-            </div>
-          )}
-        </div>
+    <div>
+      <Header />
+      <div className="container mx-auto px-4 py-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
+          <div className="flex flex-col space-y-0">
+            {/* Loop through the extraImages and display them */}
+            {product.extraImages?.map((image, index) => (
+              <div
+                key={index}
+                className={`relative aspect-square overflow-hidden rounded-lg shadow-lg ${isOutOfStock ? 'opacity-50' : ''}`}
+              >
+                <Image
+                  src={imageUrl(image).url()}
+                  alt={`${product.name} extra image ${index + 1}`}
+                  fill
+                  className="object-contain transition-transform duration-300 hover:scale-105"
+                />
+              </div>
+            ))}
 
-        <div className="flex flex-col justify-between">
-          <div>
-            <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-            <div className="text-xl font-semibold mb-4">
-              ${product.price?.toFixed(2)}
-            </div>
-            <div className="prose max-w-none mb-6">
-              {Array.isArray(product.description) && (
-                <PortableText value={product.description} />
-              )}
-            </div>
+            {isOutOfStock && (
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                <span className="text-white font-bold text-lg">
+                  Out of Stock
+                </span>
+              </div>
+            )}
           </div>
-          <div className="mt-6">
-            <AddToBasketButton product={product} disabled={isOutOfStock} />
-            <Button>add to basket</Button>
+          <div className="flex flex-col justify-between fixed bottom-0 left-0 right-0 bg-white px-4 py-6">
+            <div>
+              <h1 className="uppercase text-xl font-semibold text-center text-gray-800">
+                {product.name}
+              </h1>
+              <div className="uppercase text-lg font-light text-center text-gray-800">
+                ${product.price?.toFixed(0)}
+              </div>
+              <div className="prose max-w-none">
+                {Array.isArray(product.description) && (
+                  <PortableText value={product.description} />
+                )}
+              </div>
+            </div>
+            <div className="mt-6">
+              <AddToBasketButton product={product} disabled={isOutOfStock} />
+              <Button>add to basket</Button>
+            </div>
           </div>
         </div>
       </div>
