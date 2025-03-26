1bd15b208e5bce9d358c2b121d3953c9cf400f18
gtz.jesus <gtz.jesus@outlook.com>
2025-03-26 14:32:29 -0600

    add product functinality

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/common/header';
-// import ReadMore from '@/components/common/ReadMore';
+import DescriptionDropdown from '@/components/common/DescriptionDropdown'; // Importing the client component
 import ProductClient from '@/components/products/ProductClient';
 import { imageUrl } from '@/lib/imageUrl';
 import { getProductBySlug } from '@/sanity/lib/products/getProductBySlug';
@@ -29,7 +29,7 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
           {product.name}
         </h1>
       </div>
-      <div className="grid grid-cols-1  lg:grid-cols-2 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 ">
         <div className="flex-grow overflow-y-auto pb-40 lg:pb-0">
           {/* Loop through the extraImages and display them */}
           {product.extraImages?.map((image, index) => (
@@ -45,7 +45,9 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
               />
             </div>
           ))}
-          {/* <ReadMore description={product.description} /> */}
+
+          {/* Using the client component for the description dropdown */}
+          <DescriptionDropdown description={product.description ?? ''} />
 
           {isOutOfStock && (
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
@@ -55,7 +57,7 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
         </div>
 
         {/* Fixed Product Summary */}
-        <div className="w-full lg:w-90 lg:sticky  h-fit bg-white p-6 border order-first lg:order-last fixed bottom-0 left-0 lg:left-auto">
+        <div className="w-full lg:w-90 lg:sticky h-fit bg-white p-6 border order-first lg:order-last fixed bottom-0 left-0 lg:left-auto">
           <div className="flex justify-center items-center gap-1 ">
             <h1 className="uppercase text-md font-semibold text-center text-gray-800">
               {product.name}
