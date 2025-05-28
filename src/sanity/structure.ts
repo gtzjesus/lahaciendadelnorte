b2a86f283bf9efc53490b249eb77df5efa916af9
gtz.jesus <gtz.jesus@outlook.com>
2025-05-28 12:35:39 -0600

    Modular separation of concerns + schema for customer backend

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,13 +1,15 @@
 import type { StructureResolver } from 'sanity/structure';
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Shop Worldhello')
     .items([
       S.documentTypeListItem('category').title('Categories'),
+      S.documentTypeListItem('customer').title('Customers'), // ✅ Explicitly add customer
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category'].includes(item.getId()!)
+        (item) =>
+          item.getId() &&
+          !['post', 'category', 'customer'].includes(item.getId()!) // ✅ Avoid duplicate entry
       ),
     ]);
