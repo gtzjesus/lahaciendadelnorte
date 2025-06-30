b6753acf27b42d4d66f5310be6a874bc4438498c
gtz.jesus <gtz.jesus@outlook.com>
2025-06-29 20:13:35 -0600

    inventory manager

--- src/components/inventory/InventoryCard.tsx
+++ src/components/inventory/InventoryCard.tsx
@@ -152,7 +152,11 @@ const InventoryCard: React.FC<InventoryCardProps> = ({
           disabled={isSaving}
           className="mt-4 bg-flag-blue text-white px-4 py-2 text-xs uppercase tracking-wide"
         >
-          {isSaving ? 'Saving...' : saved ? '✔ Saved!' : 'Save Product'}
+          {isSaving
+            ? 'updating...'
+            : saved
+              ? '✔ firework updated!'
+              : 'update firework'}
         </button>
       </div>
     </div>
