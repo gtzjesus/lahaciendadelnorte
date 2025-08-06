9f18354ba57733e8383992d9a31109477719490a
gtz.jesus <gtz.jesus@outlook.com>
2025-08-06 15:18:07 -0600

    search

--- src/components/admin/pos/SaleSummary.tsx
+++ src/components/admin/pos/SaleSummary.tsx
@@ -30,30 +30,32 @@ export default function SaleSummary({
   onInputFocus?: () => void;
   onInputBlur?: () => void;
 }) {
+  const isCashValid = cashReceived >= total;
+
   return (
-    <div className="max-w-xl flex flex-col align-center p-4">
+    <div className="max-w-xl flex flex-col align-center py-2">
       <div className="flex justify-between uppercase text-xs font-semibold text-center text-black border-black border-b">
         <h3 className="mb-2">Sale Summary</h3>
         <p> Items: {totalItems}</p>
       </div>
 
       <div className="flex justify-between mt-2 text-sm uppercase font-light">
-        <p>Subtotal:</p>
+        <p className="text-xs">Subtotal:</p>
         <p>${subtotal.toFixed(2)}</p>
       </div>
 
-      <div className="flex justify-between mt-2 text-sm uppercase font-light">
-        <p>Tax:</p>
+      <div className="flex justify-between text-sm uppercase font-light">
+        <p className="text-xs">Tax:</p>
         <p>${tax.toFixed(2)}</p>
       </div>
 
-      <div className="flex justify-between mt-2 text-sm uppercase font-light">
-        <p>Total:</p>
-        <p className="text-green">${total.toFixed(2)}</p>
+      <div className="flex justify-between text-sm uppercase font-light">
+        <p className="text-xs">Total:</p>
+        <p className="text-green font-bold">${total.toFixed(2)}</p>
       </div>
 
       {/* Payment Method */}
-      <div className="mt-2 mb-2 text-black font-semibold">
+      <div className="text-xs text-black font-light">
         <div className="relative w-full">
           <select
             value={paymentMethod}
@@ -63,7 +65,7 @@ export default function SaleSummary({
               if (method === 'cash') setCardAmountAction(0);
               if (method === 'card') setCashReceivedAction(0);
             }}
-            className="appearance-none w-full p-2 pr-8 border border-black bg-white text-black text-sm uppercase focus:outline-none"
+            className="appearance-none w-full p-1 bg-white text-black text-sm uppercase focus:outline-none focus:ring-0"
             onFocus={onInputFocus}
             onBlur={onInputBlur}
           >
@@ -77,8 +79,8 @@ export default function SaleSummary({
         </div>
 
         {paymentMethod === 'cash' && (
-          <div className="mt-2">
-            <label className="text-sm">Cash Received</label>
+          <div className="uppercase">
+            <p className="text-xs py-1">Cash Received</p>
             <input
               type="number"
               min="0"
@@ -90,19 +92,30 @@ export default function SaleSummary({
               }}
               onFocus={onInputFocus}
               onBlur={onInputBlur}
-              className="w-full p-2 mt-1 text-black"
+              className="w-full p-2 text-black focus:outline-none focus:ring-0"
             />
-            <p className="text-sm mt-1">
-              Change Due:{' '}
-              <span className="font-bold">${changeGiven.toFixed(2)}</span>
-            </p>
+
+            {/* Show "Not enough funds" message if cashReceived < total */}
+            {cashReceived < total && cashReceived !== 0 && (
+              <p className="text-xs text-red-500 pt-1 font-semibold">
+                Not enough funds. Please provide more cash.
+              </p>
+            )}
+
+            {/* Show "Change Due" if enough cash is received */}
+            {cashReceived >= total && (
+              <div className="text-xs flex justify-between  py-1">
+                <p className="">Change Due: </p>
+                <p className="font-bold text-sm">${changeGiven.toFixed(2)}</p>
+              </div>
+            )}
           </div>
         )}
 
         {paymentMethod === 'split' && (
-          <div className="space-y-2">
+          <div className="uppercase">
             <div>
-              <label className="text-sm">Cash Portion</label>
+              <p className="text-xs py-1">Cash Portion</p>
               <input
                 type="number"
                 min="0"
@@ -114,11 +127,11 @@ export default function SaleSummary({
                 }}
                 onFocus={onInputFocus}
                 onBlur={onInputBlur}
-                className="w-full p-2 mt-1 text-black"
+                className="w-full p-2 text-black focus:outline-none focus:ring-0"
               />
             </div>
             <div>
-              <label className="text-sm">Card Portion</label>
+              <p className="text-xs py-1">Card Portion</p>
               <input
                 type="number"
                 min="0"
@@ -130,11 +143,11 @@ export default function SaleSummary({
                 }}
                 onFocus={onInputFocus}
                 onBlur={onInputBlur}
-                className="w-full p-2 text-black"
+                className="w-full p-2 text-black focus:outline-none focus:ring-0"
               />
             </div>
             {Math.abs(cashReceived + cardAmount - total) > 0.01 && (
-              <p className="text-sm text-yellow-300 font-semibold">
+              <p className="text-xs py-1 text-yellow-300 font-semibold">
                 Amount does not match total.
               </p>
             )}
@@ -154,7 +167,7 @@ export default function SaleSummary({
               placeholder="Enter customer name"
               onFocus={onInputFocus}
               onBlur={onInputBlur}
-              className="w-full uppercase text-xs p-2 mb-4 border border-gray-300 text-black"
+              className="w-full uppercase text-xs p-2 mb-4 border border-gray-300 text-black focus:outline-none focus:ring-0"
             />
             <p className="uppercase text-sm mb-4 text-gray-700">
               Are you sure you want to complete this sale for{' '}
@@ -187,15 +200,15 @@ export default function SaleSummary({
       )}
 
       {/* Final Action Buttons */}
-      <div className="flex gap-3 w-full mt-4">
+      <div className="flex gap-5 w-full mt-4">
         <button
           onClick={clearCartAction}
           disabled={cartEmpty || loading}
-          className={`w-full py-3 rounded-full text-sm font-semibold uppercase transition duration-200 ease-in-out shadow-sm 
+          className={`w-full py-2 rounded-full text-xs font-semibold uppercase transition duration-200 ease-in-out shadow-sm 
             ${
               cartEmpty || loading
                 ? 'bg-red-300 cursor-not-allowed text-white'
-                : 'bg-red-500 hover:bg-red-600 active:bg-red-700 text-white'
+                : 'bg-red-500  active:bg-red-700 text-white'
             }`}
         >
           Clear Sale
@@ -203,12 +216,14 @@ export default function SaleSummary({
 
         <button
           onClick={() => setShowConfirmModalAction(true)}
-          disabled={loading || cartEmpty}
-          className={`w-full py-3 rounded-full text-sm font-semibold uppercase transition duration-200 ease-in-out shadow-sm 
+          disabled={
+            loading || cartEmpty || (paymentMethod === 'cash' && !isCashValid)
+          }
+          className={`w-full py-2 rounded-full text-xs font-semibold uppercase transition duration-200 ease-in-out shadow-sm 
             ${
-              loading || cartEmpty
-                ? 'bg-gray-300 cursor-not-allowed text-gray-600'
-                : 'bg-green hover:bg-gray-800 text-white active:scale-[0.98]'
+              loading || cartEmpty || (paymentMethod === 'cash' && !isCashValid)
+                ? 'bg-gray-500 cursor-not-allowed text-white'
+                : 'bg-green  text-white active:scale-[0.98]'
             }`}
         >
           {loading ? `Processing... $${total.toFixed(2)}` : `Complete Sale`}
