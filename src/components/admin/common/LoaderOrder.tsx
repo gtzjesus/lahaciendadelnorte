50ec18ba29c1f05d011d43dc8e0eda686060ceb2
gtz.jesus <gtz.jesus@outlook.com>
2025-08-18 18:03:13 -0600

    inventory fixes

--- src/components/admin/common/LoaderOrder.tsx
+++ src/components/admin/common/LoaderOrder.tsx
@@ -12,7 +12,7 @@ export default function Loader() {
     <div className="flex flex-col gap-2 items-center justify-center min-h-screen">
       <div className="h-20 w-20 animate-spin rounded-full border-4 border-red-300 border-t-primary" />
       <h2 className="uppercase text-center text-md text-black">
-        finishing up your order! <br />
+        loading! <br />
         hang tight!
       </h2>
     </div>
