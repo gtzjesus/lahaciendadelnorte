b3857f7f613fb7d59caf9cf95172deebe5b56a7c
gtz.jesus <gtz.jesus@outlook.com>
2025-08-25 13:06:08 -0600

    not found

--- src/components/(store)/common/HeroSection.tsx
+++ src/components/(store)/common/HeroSection.tsx
@@ -10,7 +10,7 @@ interface HeroSectionProps {
 const HeroSection: FC<HeroSectionProps> = ({ className = '' }) => {
   return (
     <div
-      className={`absolute inset-0 flex flex-col justify-between items-center py-20 ${className} z-20`}
+      className={`absolute inset-0 flex flex-col justify-between items-center py-10 ${className} z-20`}
     >
       {/* Title near the top with text shadow */}
       <h1
@@ -21,15 +21,15 @@ const HeroSection: FC<HeroSectionProps> = ({ className = '' }) => {
       </h1>
 
       <p
-        className="max-w-lg font-bold text-xl lg:text-2xl text-white leading-tight text-center px-4 pt-80
+        className="max-w-lg font-bold text-xl lg:text-2xl text-white leading-tight text-center px-4
           drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
       >
         We make it simple to create the custom storage you need — built with
-        trusted craftsmanship and tailored to your lifestyle
+        trusted craftsmanship
       </p>
 
       {/* Buttons near the bottom with solid background & shadow */}
-      <div className="flex justify-center gap-2 w-full max-w-lg px-1 mb-20 ">
+      <div className="flex justify-center gap-2 w-full max-w-lg px-1 mb-20 mt-10">
         <Link
           href="/"
           className=" bg-opacity-90 border border-white px-6 py-5 text-center lg:text-lg rounded-3xl text-xs font-bold transition duration-200 ease-in-out shadow-lg text-white w-full max-w-[180px] hover:bg-opacity-100  drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
