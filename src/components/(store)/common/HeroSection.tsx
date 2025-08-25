d15c9690235da6ee2f0b77989f60999656539b11
gtz.jesus <gtz.jesus@outlook.com>
2025-08-25 12:19:29 -0600

    not found

--- src/components/(store)/common/HeroSection.tsx
+++ src/components/(store)/common/HeroSection.tsx
@@ -12,25 +12,25 @@ const HeroSection: FC<HeroSectionProps> = ({ className = '' }) => {
     <div
       className={`absolute inset-0 flex flex-col justify-between items-center py-20 ${className} z-20`}
     >
-      {/* Optional red tint behind text, above overlay */}
-      {/* <div className="absolute inset-0 bg-flag-red opacity-50 -z-10" /> */}
-
-      {/* Title near the top */}
-      <h1 className="uppercase font-bold text-5xl lg:text-8xl text-white leading-tight text-center px-4 mt-20">
+      {/* Title near the top with text shadow */}
+      <h1
+        className="uppercase font-bold text-5xl lg:text-8xl text-white leading-tight text-center px-4 mt-20
+          drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
+      >
         Build your <br /> Custom Storage
       </h1>
 
-      {/* Buttons near the bottom */}
-      <div className="flex justify-center gap-2 w-full max-w-lg px-4 mb-20">
+      {/* Buttons near the bottom with solid background & shadow */}
+      <div className="flex justify-center gap-4 w-full max-w-lg px-4 mb-20 ">
         <Link
           href="/"
-          className="border px-6 py-5 text-center lg:text-lg rounded-3xl text-xs font-bold transition duration-200 ease-in-out shadow-sm text-white w-full max-w-[180px]"
+          className=" bg-opacity-90 border border-white px-6 py-5 text-center lg:text-lg rounded-3xl text-xs font-bold transition duration-200 ease-in-out shadow-lg text-white w-full max-w-[180px] hover:bg-opacity-100  drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
         >
           Qualify Here
         </Link>
         <Link
           href="/"
-          className="border px-6 py-5 text-center lg:text-lg rounded-3xl text-xs font-bold transition duration-200 ease-in-out shadow-sm text-white w-full max-w-[180px]"
+          className=" bg-opacity-90 border border-white px-6 py-5 text-center lg:text-lg rounded-3xl text-xs font-bold transition duration-200 ease-in-out shadow-lg text-white w-full max-w-[180px] hover:bg-opacity-100  drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
         >
           Build Storage
         </Link>
