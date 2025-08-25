1a6507081a4b1ebbf4cd16b9bdcc01760297fc99
gtz.jesus <gtz.jesus@outlook.com>
2025-08-25 12:49:44 -0600

    not found

--- src/components/(store)/common/Footer.tsx
+++ src/components/(store)/common/Footer.tsx
@@ -1,5 +1,7 @@
 // components/Footer.tsx
 
+import Image from 'next/image';
+
 /**
  * Footer component displays the site’s branding and copyright info.
  *
@@ -11,11 +13,24 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex flex-col pt-5 text-flag-blue overflow-hidden">
       <div>
-        <p className="uppercase text-center text-sm tracking-very-wide font-light ">
+        <p
+          className="uppercase font-bold text-sm leading-tight text-center my-2 px-1 
+          drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)] "
+        >
           Serving El Paso <br /> and surrounding areas
         </p>
+        <div className="flex justify-center">
+          <Image
+            src="/icons/logo.webp"
+            alt="lahacienda"
+            width={25}
+            height={25}
+            priority
+            className="my-3"
+          />
+        </div>
         <h1
-          className="uppercase font-bold text-4xl  text-white leading-tight text-center px-1 
+          className="uppercase font-bold text-3xl  text-white leading-tight text-center mb-4 px-1 
           drop-shadow-[0_4px_6px_rgba(0,0,0,0.9)]"
         >
           Hacienda del norte
@@ -23,7 +38,7 @@ const Footer: React.FC = () => {
       </div>
 
       {/* Copyright */}
-      <p className="uppercase text-center text-xs tracking-very-wide font-light mb-5 ">
+      <p className="uppercase text-center text-xs tracking-very-wide font-light mb-2 ">
         &copy; la hacienda del norte {new Date().getFullYear()}.
       </p>
     </footer>
